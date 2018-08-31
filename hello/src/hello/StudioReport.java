9ad9fc7982a27a1c9b54bdb123b71b23e99a96cc
sigylon <20855902@qq.com>
2018-08-31 16:06:09 +0800

    发布项目

---
+++ hello/src/hello/StudioReport.java
@@ -0,0 +1,30 @@
+package hello;
+
+import java.util.Scanner;
+
+public class StudioReport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in=new Scanner(System.in);
+		int r;
+		int s;
+		r=in.nextInt();
+		s=in.nextInt();
+		if((r>=1)&&(r<=5)&&(s>=1)&&(s<=9))
+		{
+			
+		
+		}
+		else 
+		{
+			System.out.print("ÊäÈë´íÎó");
+			
+		}
+		
+		
+
+	}
+
+}
---
+++ hello/src/hello/比较.java
@@ -0,0 +1,60 @@
+package hello;
+
+import java.util.Scanner;
+
+public class ±»Ωœ {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in=new Scanner(System.in);
+		int bjt;
+		int utc;
+		int s,m;
+		bjt=in.nextInt();
+		s=bjt%10;
+		m=bjt%100/10;
+		
+		if((bjt>=0&&bjt<=2359))
+		{
+			if((bjt/100)>8) {
+				utc=((bjt/100)-8);
+				System.out.print(utc);
+				System.out.print(m);
+				System.out.print(s);
+			}
+			else if((bjt/100)==8) {
+				 if(m>0) {
+					System.out.print(m);
+					System.out.print(s);
+				}else
+					System.out.print(s);
+				
+				   
+				}
+			
+			else if((bjt/100)<8) {
+				utc=((bjt/100)+24-8);
+				System.out.print(utc);
+				System.out.print(m);
+				System.out.print(s);
+			}
+			else if(((bjt/100)==0)&&(m>0)) {
+				
+				System.out.print(m);
+				System.out.print(s);
+			}
+			
+            else if(((bjt/100)==0)&&(m==0)) {
+				
+				
+				System.out.print(s);
+			}
+			}else {
+				System.out.println(" ‰»Î¥ÌŒÛ");
+			}
+		}
+		
+	    }  
